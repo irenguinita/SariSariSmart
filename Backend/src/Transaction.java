422c2d2841dbcfc285c975b8e7ead8e29c5bf59e
irenguinita <guinitairen@gmail.com>
2025-11-26 23:59:26 +0800

    separated front and backend

---
+++ Backend/src/Transaction.java
@@ -0,0 +1,19 @@
+import java.util.Date;
+import java.util.List;
+
+class Transaction {
+    String id;
+    Date date;
+    List<CartItem> items;
+    double total;
+    String customerId, customerName;
+
+    public Transaction(String id, Date date, List<CartItem> items, double total, String customerId, String customerName) {
+        this.id = id;
+        this.date = date;
+        this.items = items;
+        this.total = total;
+        this.customerId = customerId;
+        this.customerName = customerName;
+    }
+}
